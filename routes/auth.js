518d5eb015b6e45aa004e653d04426e25283c214
ruchipratihast <ruchipratihast99@gmail.com>
2024-01-20 20:03:00 +0530

    added error handler middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const {register,login} = require('../controllers/authController');
+const {verifyJwt} = require('../middlewares/authMiddleware')
 
 router.post('/register',register);
 router.post('/login',login);
